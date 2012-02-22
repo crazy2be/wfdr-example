74e32072fb287f6e258a4d6d6687dfbdb9ef6179
crazy2be <crazy1be@gmail.com>
2012-02-22 10:57:27 -0500

    Changed news and pages modules to use the default PageServer functions now available in wfdr/pages

--- modules/news/src/news.go
+++ modules/news/src/news.go
@@ -4,43 +4,39 @@ import (
 	"fmt"
 	"net/http"
 
-	"wfdr/perms"
 	"wfdr/pages"
 	"wfdr/template"
 )
 
-var defaultPS pages.PageServer = &pages.PageServer{Prefix: "/news/"}
+var defaultPS *pages.PageServer = &pages.PageServer{Prefix: "/news/", PageAlias: "Article", Manager: &pages.FSManager{"data/pages/title", "data/pages/content"}}
 
 func Handler(c http.ResponseWriter, r *http.Request) {
+	rp := r.URL.Path
 	if r.Method == "POST" {
-		defaultPS.SaveHandler(c, r)
+		defaultPS.Save(c, r)
 		return
 	}
-	rp := r.URL.Path
+	if rp == "/news/" {
+		defaultPS.List(c, r)
+		return
+	}
 	// /news/<article>/edit triggers this
 	if rp[len(rp)-len("/edit"):] == "/edit" {
-		defaultPS.EditHandler(c, r)
+		defaultPS.Edit(c, r)
 		return
 	}
-	if len(rp) != len("/news/") {
-		defaultPS.ArticleHandler(c, r)
-		return
-	}
-
-	plist, err := pages.List()
-	if err != nil {
-		template.Error500(c, r, err)
+	if rp == "/news/add" {
+		defaultPS.Add(c, r)
 		return
 	}
 	
-	template.Render(c, r, "News", "main", plist)
+	defaultPS.Display(c, r)
 	return
 }
 
 func main() {
 	fmt.Printf("Loading news server...\n")
 	template.SetModuleName("news")
-	http.HandleFunc("/news/add", AddHandler)
 	http.HandleFunc("/news/", Handler)
 	http.ListenAndServe(":8170", nil)
 }
--- modules/pages/src/main.go
+++ modules/pages/src/main.go
@@ -2,79 +2,31 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
-	"wfdr/perms"
 	"wfdr/pages"
 	tmpl "wfdr/template"
 )
 
-func SaveHandler(c http.ResponseWriter, r *http.Request) {
-	p := perms.GetPerms(r)
-	if !p.Write {
-		fmt.Fprintln(c, "Access Denied")
-		return
-	}
-
-	name := r.URL.Path[len("/pages/"):]
-
-	content := r.FormValue("content")
-	title := r.FormValue("title")
-
-	err := pages.Save(name, title, []byte(content))
-	if err != nil {
-		tmpl.Error500(c, r, err)
-		return
-	}
-	http.Redirect(c, r, "/pages/"+name, 301)
-}
-
-func EditHandler(c http.ResponseWriter, r *http.Request) {
-	name := r.URL.Path[len("/pages/") : len(r.URL.Path)-len("/edit")]
-	log.Println("Editing page:", name)
-	
-	page, err := pages.Load(name)
-	if err != nil {
-		tmpl.Error500(c, r, err)
-		return
-	}
-
-	log.Println("Request for pages server. Responding.")
-	
-	tmpl.Render(c, r, "Editing "+page.Title, "edit", page)
-}
-
-func ListHandler(c http.ResponseWriter, r *http.Request) {
-	plist := pages.List()
-	tmpl.Render(c, r, "Pages list", "list", plist)
-	return
-}
+var defaultManager pages.Manager = &pages.FSManager{"data/pages/title", "data/pages/content"}
+var defaultPS *pages.PageServer = &pages.PageServer{Prefix: "/pages/", PageAlias: "Page", Manager: defaultManager}
 
 func Handler(c http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
-		SaveHandler(c, r)
+		defaultPS.Save(c, r)
 		return
 	}
 	rp := r.URL.Path
 	if rp[len(rp)-len("/edit"):] == "/edit" {
-		EditHandler(c, r)
+		defaultPS.Edit(c, r)
 		return
 	}
 	if rp == "/pages/" {
-		ListHandler(c, r)
+		defaultPS.List(c, r)
 		return
 	}
-
-	pageData, err := pages.Load(r.URL.Path[len("/pages/"):])
-	if err != nil {
-		// TODO: 404 Error
-		tmpl.Error404(c, r, err)
-		return
-	}
-	log.Println("Request for pages server. Responding.")
-	
-	tmpl.Render(c, r, pageData.Title, "main", pageData)
+	defaultPS.Display(c, r)
+	return
 }
 
 func main() {
